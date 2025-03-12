e03fbce4a22b519c06f130b8cacfc1cfeb295fd8
Heloiza Mendes <helocursos@gmail.com>
2025-03-12 00:25:05 -0300

    refatoração da tabela usuários

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from create_table_users import create_table_usuarios
+from create_users_table import create_users_table
 
 
 if __name__ == "__main__":
     print("Criando todas as tabelas no banco de dados...")
-    create_table_usuarios()
+    create_users_table()
     print("Todas as tabelas foram criadas com sucesso!")
